ea2ba45c2eac252ed297a92117f0dc03e6474e7c
nuranzakria99 <nuranzakria99@gmail.com>
2025-01-13 14:06:41 +0200

    discover layout

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,5 +1,6 @@
 import { HiOutlineHashtag, HiOutlineHome, HiOutlinePhotograph, HiOutlineUserGroup } from 'react-icons/hi';
 
+// Array with aobjects has two keys
 export const genres = [
   { title: 'Pop', value: 'POP' },
   { title: 'Hip-Hop', value: 'HIP_HOP_RAP' },
